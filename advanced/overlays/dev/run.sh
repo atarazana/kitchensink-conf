92577a769f900bbb6af39fc8ad5053cc343f1ae2
cvicens <carlos.vicens.alonso@gmail.com>
2023-06-16 10:30:07 +0200

    fix image-puller rb points to dev and test namespaces through values

--- advanced/overlays/dev/run.sh
+++ advanced/overlays/dev/run.sh
@@ -2,5 +2,5 @@
 ARGOCD_APP_NAME=events
 BASE_REPO_URL=https://github.com/atarazana/gramola
 NAMESPACE_SUFFIX="-user1"
-BASE_NAMESPACE=demo-7-dev
+BASE_NAMESPACE=demo-6
 helm template ../../helm_base --name-template $ARGOCD_APP_NAME --set debug=${DEBUG},baseNamespace=${BASE_NAMESPACE},baseRepoUrl=${BASE_REPO_URL} --include-crds > ../../helm_base/all.yml && kustomize build | sed "s/\(namespace:[[:space:]]\{1,\}\)\(gramola-.*\)/\1\2${NAMESPACE_SUFFIX}/"
